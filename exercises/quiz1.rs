a553493cf0cdec364db87669e93bca808e3b6ff4
gamendez98 <36478111+gamendez98@users.noreply.github.com>
2024-02-18 23:46:24 +0000

    some more

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,12 +13,18 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
 
 // Don't modify this function!
+
+fn calculate_price_of_apples(amount : i32) -> i32{
+    if amount <= 40 {
+        return amount*2;
+    }
+    amount
+}
+
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
